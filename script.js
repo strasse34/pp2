209b5b87425df77a6a6da87ae36024ef4b96f869
strasse34 <rmirzaie2021@gmail.com>
2023-05-05 17:00:24 +0000

    table for each entered and missed were merged. js: now i can get words and its meaning in order in page by clicking on derButton.

--- script.js
+++ script.js
@@ -1,68 +1,106 @@
 // starting game
 document.addEventListener('DOMContentLoaded', function(){
-    // selecting no. of the words from the table in order
-    let findNumber = document.querySelectorAll("#ent-der td:nth-child(1)");
-    let numberList = [];    
-    for (let i = 0; i < findNumber.length; i++ ){            
-        let number = findNumber[i].textContent;
-        numberList.push(number);
-        }
-    // selecting words from the table in order with their no.
-        let wordList = [];
-    if (number === [i] + 1){
-        let findWord = document.querySelectorAll("#ent-der td:nth-child(3)");        
-        for (let j = 0; j < findWord.length; j++){
-            let newWord = findWord[j].textContent;       
-            wordList.push(newWord);
-            }
-        // displaying the selected word in the page
-            let answer = document.getElementById('answer');
-        answer.textContent = newWord[j];
-    } 
-    runGame();   
+    // getting word from table
+    let currentRow = 0;
+    let wordList =[];
+    let meaningList = [];
+
+    function word(){
+    let findWord = document.querySelectorAll("#ent-der td:nth-child(3)");   
+    for (i = 0; i < findWord.length; i++){
+        let word = findWord[i].textContent; 
+        wordList.push(word);       
+        }   
+    }
+    // getting words' meaning
+    function meaning(){ 
+    let findMeaning = document.querySelectorAll("#ent-der td:nth-child(4)");   
+        for (i = 0; i <findMeaning.length; i++){
+        let meaning = findMeaning[i].textContent;
+        meaningList.push(meaning);
+        }    
+    }
+    // displaying word and meaning in page
+    let answer = document.getElementById('answer');
+    answer.textContent = wordList[currentRow];
+    let engMeaning = document.getElementById('eng-meaning')
+    engMeaning.textContent = meaningList[currentRow];
+
+    word();
+    meaning();
+
+    let derButton = document.getElementById('der');
+    derButton.addEventListener('click', function() {
+    currentRow++;
+    if (currentRow >= wordList.length) {
+      currentRow = 0;
+    }
+    answer.textContent = wordList[currentRow];
+    engMeaning.textContent = meaningList[currentRow];
+})
+    
 })
 
 
-function runGame(){   
-    let findArticle = document.querySelectorAll("#ent-der td:nth-child(2)");
-    let articleList = [];    
-    for (let i = 0; i < findArticle.length; i++ ){            
-        let article = findArticle[i].textContent;
-        articleList.push(article);
-        } 
-    
-        let buttoms = document.getElementsByTagName ('button');
-    for (button of buttoms){
-        addEventListener('click', runGame());
-    }
-    if ()
-    let derButton = document.getElementById('der');
-    let dieButton = document.getElementById('die');
-    let dasButton = document.getElementById('das');
+// function continueGame(){   
+//     word();
+//     meaning();
+//     }
+        
 
-}
 
-function getAnswer(){
 
-}
+// function runGame(){   
+//     let findArticle = document.querySelectorAll("#ent-der td:nth-child(2)");
+//     let articleList = [];    
+//     for (let i = 0; i < findArticle.length; i++ ){            
+//         let article = findArticle[i].textContent;
+//         articleList.push(article);
+//         } 
+//     let derButton = document.getElementById('der');
+//     let dieButton = document.getElementById('die');
+//     let dasButton = document.getElementById('das');
+//     if (derButton.addEventListener('click')){
+//         if(derButton.textContent === article[i]){            
+//             incrementGreen(); 
+//             continueGame();
+//         } else {
+//             setTimeout(article.style.backgroundColor = 'red' , 1000);
+//             incrementRed();
+//             continueGame();
+//         }
+//     }
+//     if (dieButton.addEventListener('click')){
+//         if(dieButton.textContent === article[i]){
+//             incrementGreen();
+//             continueGame();
+//         } else {
+//             setTimeout(article.style.backgroundColor = 'red' , 1000);
+//             incrementRed();
+//             continueGame();        
+//         }
+//     }
+//     if (dasButton.addEventListener('click')){
+//         if(dasButton.textContent === article[i]){
+//             incrementGreen();
+//             continueGame();
+//         } else {
+//             setTimeout(article.style.backgroundColor = 'red' , 1000);
+//             incrementRed();
+//             continueGame();
+//         }
+//     }
+// }
 
-function checkAnswer(){
+// function incrementGreen(){
+   
+// }
 
-}
+// function incrementRed(){
 
-function answerFeedback(){
+// }
 
-}
+// function practiceRed(){
 
-function incrementGreen(){
-
-}
-
-function incrementRed(){
-
-}
-
-function practiceRed(){
-
-}
+// }
 
