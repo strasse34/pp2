fb87332af9684650c5234c3cf0c327552ab02ea2
strasse34 <rmirzaie2021@gmail.com>
2023-05-05 18:37:15 +0000

    js: I could not make buttons function.

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // starting game
 document.addEventListener('DOMContentLoaded', function(){
     // getting word from table
-    let currentRow= 0;    
+    let currentRow= 0;
+    let articleList = [];    
     let wordList =[];
     let meaningList = [];
    
     // getting words
     function word(){
-    let findWord = document.querySelectorAll("#ent-words td:nth-child(2)");   
-    for (i = 0; i < findWord.length; i++){
+        let findWord = document.querySelectorAll("#ent-words td:nth-child(2)");   
+        for (i = 0; i < findWord.length; i++){
         let word = findWord[i].textContent; 
         wordList.push(word);       
         } 
@@ -17,72 +18,70 @@ document.addEventListener('DOMContentLoaded', function(){
     }
     // getting words' meaning
     function meaning(){ 
-    let findMeaning = document.querySelectorAll("#ent-words td:nth-child(3)");   
+        let findMeaning = document.querySelectorAll("#ent-words td:nth-child(3)");   
         for (i = 0; i <findMeaning.length; i++){
         let meaning = findMeaning[i].textContent;
         meaningList.push(meaning);
         }
         let engMeaning = document.getElementById('eng-meaning');
         engMeaning.textContent = meaningList[currentRow];           
-    }    
+    } 
+    // getting article
+    function article(){
+        let findArticle = document.querySelectorAll("#ent-words td:nth-child(1)");        
+        for (let i = 0; i < findArticle.length; i++ ){            
+            let article = findArticle[i].textContent;
+            articleList.push(article);
+            }        
+        }   
 
-    function derButton(){
-        let articleList = [];
-        function article(){
-            let findArticle = document.querySelectorAll("#ent-words td:nth-child(1)");        
-            for (let i = 0; i < findArticle.length; i++ ){            
-                let article = findArticle[i].textContent;
-                articleList.push(article);
-                } 
-            }
-        article();
-        let article = document.getElementById('article')
+    function derButton(){       
         let derButton = document.getElementById('der');
-        derButton.addEventListener('click', function() {
-            currentRow++;
-            if (currentRow >= wordList.length) {
-            currentRow = 0;
-            }
-            let answer = document.getElementById('answer');
-            answer.textContent = wordList[currentRow];
-            let engMeaning = document.getElementById('eng-meaning');
-            engMeaning.textContent = meaningList[currentRow];
-            })
+        derButton.addEventListener('click', function(){
+            let article = document.getElementById('article');
+            displayArticle(article);
+        }) 
     }
     function dieButton(){
+        let dieButton = document.getElementById('der');
+        dieButton.addEventListener('click', function(){
+            let article = document.getElementById('article');
+            displayArticle(article);
+        })
         
-        let dieButton = document.getElementById('die');
-        dieButton.addEventListener('click', function() {
-            currentRow++;
-            if (currentRow >= wordList.length) {
-            currentRow = 0;
-            }
-            let answer = document.getElementById('answer');
-            answer.textContent = wordList[currentRow];
-            let engMeaning = document.getElementById('eng-meaning');
-            engMeaning.textContent = meaningList[currentRow];
-            })
     }
     function dasButton(){
-        let dasButton = document.getElementById('das');
-        dasButton.addEventListener('click', function() {
-            currentRow++;
-            if (currentRow >= wordList.length) {
-            currentRow = 0;
-            }
-            let answer = document.getElementById('answer');
-            answer.textContent = wordList[currentRow];
-            let engMeaning = document.getElementById('eng-meaning');
-            engMeaning.textContent = meaningList[currentRow];
-            })
+        let dasButton = document.getElementById('der');
+        dasButton.addEventListener('click', function(){
+            let article = document.getElementById('article');
+            displayArticle(article);
+        })
     }
+
+    function displayArticle(){
+        let articleElement = document.addEventListener('article');
+        article.textContent = article;
+        setTimeout(function(){
+            nextPair(); }, 2000);
+        }
+
+    function nextPair(){
+        currentRow++;
+        if (currentRow >= wordList.length){
+            currentRow = 0;
+        }
+        word();
+        meaning();
+    }
+    
+    article();
     word();
     meaning();
     derButton();
     dieButton();
     dasButton();   
     
-})
+});
 
 
 
